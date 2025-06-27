96d685ea3edaf041ca5ea15fcae8d5b70061742b
hafizkashifdev <kashifnazim127@gmail.com>
2025-06-27 12:24:08 +0500

    fa

---
+++ src/app/(pages)/mental-health/page.tsx
@@ -0,0 +1,12 @@
+
+
+import { AboutUsSection } from '@/features/about-us/about-us-section'
+import React from 'react'
+
+const MentalHealthPage = () => {
+  return (
+    < AboutUsSection/>
+  )
+}
+
+export default MentalHealthPage
---
+++ src/assets/index.tsx
@@ -0,0 +1,1 @@
+export { default as IMAGESample } from "./Section.svg";
