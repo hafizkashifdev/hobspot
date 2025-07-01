1071d61403d31ab6312688ae6aed40bcb86041ac
hafizkashifdev <kashifnazim127@gmail.com>
2025-07-01 09:43:09 +0500

    asa

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ export const metadata = {
   openGraph: {
     title: "Mental Health",
     description: "Concise visual breakdowns and structured diagrams for clear, accessible insight.",
-    url: "https://courthearing.netlify.app/mental-health",
+    url: "https://courthearing.netlify.app/generalized-anxiety-disorder",
     type: "website",
     images: [
       {
