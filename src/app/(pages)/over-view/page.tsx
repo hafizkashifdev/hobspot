5961af3b6d9422c37b9f5872679ca50a3f09e4e7
hafizkashifdev <kashifnazim127@gmail.com>
2025-07-01 14:39:35 +0500

    fix og

--- src/app/(pages)/over-view/page.tsx
+++ src/app/(pages)/over-view/page.tsx
@@ -1,3 +1,22 @@
+
+export const metadata = {
+  title: "Mental Health",
+  description: "Concise visual breakdowns and structured diagrams for clear, accessible insight.",
+  openGraph: {
+    title: "Mental Health",
+    description: "Concise visual breakdowns and structured diagrams for clear, accessible insight.",
+    url: "https://courthearing.netlify.app/",
+    type: "website",
+    images: [
+      {
+        url: "https://courthearing.netlify.app/og.png",
+        width: 1200,
+        height: 630,
+        alt: "Mental Health",
+      },
+    ],
+  },
+};
 import React from "react";
 import { section10 } from "@/assets";
 import { CommonPage } from "@/components";
