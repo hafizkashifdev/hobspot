bd50f35fa6bd483b7b13f5f4e696bf0fc481e25d
hafizkashifdev <kashifnazim127@gmail.com>
2025-07-09 16:46:35 +0500

    K

--- src/app/(pages)/fca-overview/page.tsx
+++ src/app/(pages)/fca-overview/page.tsx
@@ -8,8 +8,8 @@ const FcaOverviewPage = () => {
     <AmendmentComparison
       pageTitle={`Mental Health / Mental Health  Act 1983 / Part VII / FCA Overview`}
       src={FcaOverviewImage}
-      backRoute="/mental-health-act-1983"
-      amendmentButtonRoute="/removal-to-and-from-scotland-ac"
+      backRoute="/fca-application"
+      amendmentButtonRoute="/fca-overview"
     />
   );
 };
