05b71b7f6cd03c91caccc71fdd757c21f483c368
hafizkashifdev <kashifnazim127@gmail.com>
2025-07-14 13:59:52 +0500

    D

--- src/assets/index.tsx
+++ src/assets/index.tsx
@@ -1,8 +1,9 @@
+
+// image onebackroute_oneImg.js
 export { default as CareQualityCommissionCqcOverviewImage } from "./CareQualityCommissionCqcOverviewImage.svg";
-export { default as WithdrawalOfApplicationImage } from "./WithdrawalOfApplicationImage.svg";
+
+// images multi-page-scricpt.js
 export { default as CqcFundamentalStandardsImage } from "./CqcFundamentalStandardsImage.svg";
-export { default as CqcGlossaryImage } from "./CqcGlossaryImage.svg";
-export { default as CrossSectorInspectionsImage } from "./CrossSectorInspectionsImage.svg";
 export { default as img1ScopeOfRegistrationImage } from "./img1ScopeOfRegistrationImage.svg";
 export { default as img2ScopeOfRegistrationImage } from "./img2ScopeOfRegistrationImage.svg";
 export { default as img3ScopeOfRegistrationImage } from "./img3ScopeOfRegistrationImage.svg";
