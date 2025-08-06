9b1e5078f9579dae27236bc309de4b24ad294e0e
hafizkashifdev <kashifnazim127@gmail.com>
2025-08-06 15:43:06 +0500

    DD

--- next.config.ts
+++ next.config.ts
@@ -2,20 +2,18 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   typescript: {
+    // Ignore TypeScript errors during build
     ignoreBuildErrors: true,
   },
   eslint: {
+    // Ignore ESLint errors during build
     ignoreDuringBuilds: true,
+  },
+  // Optional: Reduce Next.js build size & improve deploy speed
+  experimental: {
+    optimizeCss: true,
+    scrollRestoration: true,
   },
 };
 
 export default nextConfig;
-
-
-// import type { NextConfig } from "next";
-
-// const nextConfig: NextConfig = {
-//   /* config options here */
-// };
-
-// export default nextConfig;
